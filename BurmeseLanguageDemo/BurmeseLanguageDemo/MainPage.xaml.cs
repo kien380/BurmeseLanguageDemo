63de16ce6ada73e407367140e96c482c7d04794d
Kienhht-InfoCorp <kienhht@infocorp.io>
2018-06-13 16:41:25 +0700

    Init Source code

---
+++ BurmeseLanguageDemo/BurmeseLanguageDemo/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace BurmeseLanguageDemo
+{
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LabelUnicode.Text = LabelZawgyi.Text = e.NewTextValue;
+        }
+
+        private void TextChanged2(object sender, TextChangedEventArgs e)
+        {
+            LabelDisplay.Text = LabelDisplayBold.Text = LabelDisplayItalic.Text = e.NewTextValue;
+        }
+
+	    private void Switch_OnToggled(object sender, ToggledEventArgs e)
+	    {
+	        if (Toggle.IsToggled)
+	        {
+                LabelDisplay.FontFamily = Device.RuntimePlatform == Device.Android ? "ZawgyiOne.ttf#Zawgyi-One" : "Zawgyi-One";
+                LabelDisplayBold.FontFamily = Device.RuntimePlatform == Device.Android ? "ZawgyiOne.ttf#Zawgyi-One" : "Zawgyi-One";
+                LabelDisplay.FontFamily = Device.RuntimePlatform == Device.Android ? "ZawgyiOne.ttf#Zawgyi-One" : "Zawgyi-One";
+            }
+	        else
+	        {
+	            LabelDisplay.FontFamily = "";
+	            LabelDisplayBold.FontFamily = "";
+	            LabelDisplayItalic.FontFamily = "";
+            }
+	    }
+	}
+}
